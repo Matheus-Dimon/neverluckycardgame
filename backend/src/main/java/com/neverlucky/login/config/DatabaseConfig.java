27ed769780236bd1c83a2d8d9546c13685a6b283
eloas77 <matheus.dimon@gmail.com>
2026-01-31 18:10:39 -0300

    data base config h2 exclusion

--- backend/src/main/java/com/neverlucky/login/config/DatabaseConfig.java
+++ backend/src/main/java/com/neverlucky/login/config/DatabaseConfig.java
@@ -10,18 +10,6 @@ import java.net.URI;
 
 @Configuration
 public class DatabaseConfig {
-
-    @Bean
-    @Profile("!production")
-    public DataSource h2DataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:testdb");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("");
-        return dataSource;
-    }
-
     @Bean
     @Profile("production")
     public DataSource postgresqlDataSource() {
